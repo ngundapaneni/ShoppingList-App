aa73830a9f101cf988797640fbf054dc4d45ed2d
Natasha <ngundapaneni@gmail.com>
2014-10-24 15:46:48 -0500

    Shopping List App
    Initial check in of Shopping List App

---
+++ js/shoppingList.js
@@ -0,0 +1,40 @@
+var counter = 0;
+$(document).ready(function() {
+	/*
+		$("#btnImg").on("click", function() {
+			showListItems(counter);
+			counter = counter;
+		}
+	*/
+	$(".inputDiv input#enteredItem").on("keypress", function(event) {
+		if(event.which == 13) {
+			if($("#enteredItem").val() == "") {
+				alert("Item cannot be empty");
+			}
+			else {
+				showListItems(counter);
+				counter = counter;
+			}
+		}
+	});
+
+	$("#listItems").on("click", "li", function() {
+		$(this).toggleClass("strikeThrough");
+	});
+
+	$("#deleteBtn").on("click", function() {
+		var checkedstuff = $(this).closest('.inputDiv').find('.checkedItem:checked').parent();
+ 		checkedstuff.hide();
+	});
+});
+
+function showListItems() {
+	counter = counter + 1;
+	var enteredText = $("#enteredItem").val();
+	var item = "<div id='listContainer'><input class='checkedItem' type='checkbox' id='chkBox" + counter + "'/><li class='listItem' id='item" + counter + "' >" + enteredText + "</li></div>";
+
+	$("#listItems").append(item);
+	$(".shopListItems").show();
+	$("#enteredItem").val("");
+}
+
